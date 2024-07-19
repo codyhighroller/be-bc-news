1cfa3336b448036964b90536df4b3beffaa0862e
codyhighroller <123840075+codyhighroller@users.noreply.github.com>
2024-07-19 19:07:52 +0100

    adjust test logic for api/articles test 1

--- __tests__/get.test.js
+++ __tests__/get.test.js
@@ -95,18 +95,19 @@ describe("GET /api/articles", () => {
 			.get("/api/articles")
 			.expect(200)
 			.then(({ body }) => {
-				expect(body.articles[0]).toEqual(
-					expect.objectContaining({
-						title: expect.any(String),
-						topic: expect.any(String),
+				body.articles.forEach((article) => {
+					expect(article).toMatchObject({
 						author: expect.any(String),
+						title: expect.any(String),
 						article_id: expect.any(Number),
+						topic: expect.any(String),
 						created_at: expect.any(String),
 						votes: expect.any(Number),
 						article_img_url: expect.any(String),
 						comment_count: expect.any(Number),
-					})
-				);
+					});
+					expect(article.body).toBeUndefined();
+				});
 			});
 	});
 	test("GET:200 sends an array of all articles with the correct properties", () => {
